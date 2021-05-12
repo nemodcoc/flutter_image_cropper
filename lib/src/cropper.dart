3e1a8411874d6af2dfac5c89819d9d9df6c24b91
nemodcoc <84010382+nemodcoc@users.noreply.github.com>
2021-05-12 16:12:34 +0200

    Update cropper.dart

--- lib/src/cropper.dart
+++ lib/src/cropper.dart
@@ -149,9 +149,9 @@ class ImageCropper {
   /// permanent (if needed).
   ///
   static Future<CropInfo> cropImageWithCoordinates({
-    String sourcePath,
-    int maxWidth,
-    int maxHeight,
+    String sourcePath='',
+    int maxWidth=0,
+    int maxHeight=0,
     CropAspectRatio aspectRatio,
     List<CropAspectRatioPreset> aspectRatioPresets = const [
       CropAspectRatioPreset.original,
