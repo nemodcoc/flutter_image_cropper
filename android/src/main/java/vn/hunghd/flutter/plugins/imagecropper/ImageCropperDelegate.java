5d740f28dab42ea2d8975ed742cb5ebae02e1de9
nemodcoc <84010382+nemodcoc@users.noreply.github.com>
2021-05-14 10:26:36 +0200

    Update ImageCropperDelegate.java

--- android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
+++ android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
@@ -103,11 +103,11 @@ public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
 
             final int height = UCrop.getOutputImageHeight(data);
 
-            final float angle=0;
+            final int angle=123;
             //final float angle=   UCrop.getOutputImageAngle(data);
    
             finishWithSuccess(String.format(
-					"%s|\\|%d|\\|%d|\\|%d|\\|%d|\\|%f",
+					"%s|\\|%d|\\|%d|\\|%d|\\|%d|\\|%i",
                     fileUtils.getPathFromUri(activity, resultUri),
                     x,
                     y,
