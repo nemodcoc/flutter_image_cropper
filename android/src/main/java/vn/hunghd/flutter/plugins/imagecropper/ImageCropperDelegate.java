9e7b6374e84e52a90308d6292f38a2f5376c7839
nemodcoc <84010382+nemodcoc@users.noreply.github.com>
2021-05-25 15:56:31 +0200

    Update ImageCropperDelegate.java

--- android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
+++ android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
@@ -107,12 +107,8 @@ public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
 
    
 
-		int angle = (int)Math.round(angle2);
-		
-		final String TAG = "___flutter_image_cropper__";
-		Log.v(TAG, "angle=" + angle);
+		int angle = (int)Math.round(angle2);	
 	
-		
             finishWithSuccess(String.format(
 					"%s|\\|%d|\\|%d|\\|%d|\\|%d|\\|%d",
                     fileUtils.getPathFromUri(activity, resultUri),
