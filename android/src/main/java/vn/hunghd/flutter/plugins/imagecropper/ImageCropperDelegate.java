fc4efc2044fc1a94a7ab147e8339e8b4d1a33b9e
PyGitPyHub <85502183+PyGitPyHub@users.noreply.github.com>
2021-06-30 17:47:50 +0200

    Update ImageCropperDelegate.java

--- android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
+++ android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
@@ -103,7 +103,7 @@ public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
 
             final int height = UCrop.getOutputImageHeight(data);
 		
-	final double fAngle=   UCrop.getOutputImageAngle(data);
+	final float fAngle=   UCrop.getOutputImageAngle(data);
 
    
 
