8228f0c676da8250c4eafcfb8de8f49286ceb44a
PyGitPyHub <85502183+PyGitPyHub@users.noreply.github.com>
2021-06-30 18:27:24 +0200

    Update cropper.dart

--- lib/src/cropper.dart
+++ lib/src/cropper.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//_______________________ VERSION 4.0.1 _____________________________--
+//_______________________ VERSION 4.0.2 _____________________________--
 
 import 'dart:async';
 import 'dart:io';
